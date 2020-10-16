da51ce31f5f6ee16d7f46ca1aa76132ce71488cb
Justin Jao <jao.c.justin@gmail.com>
2020-10-15 21:27:32 -0700

    fixed checkstyle issues

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,6 +1,8 @@
 package ui;
 
 public class Main {
-    public static void main(String[] args) { new CatalogueSystem(); }
+    public static void main(String[] args) {
+        new CatalogueSystem();
+    }
 }
 
